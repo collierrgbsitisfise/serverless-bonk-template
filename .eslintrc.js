57f1bb873e5501e06069ec353978371385eee696
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2021-02-10 22:24:18 +0200

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: './',
+    sourceType: 'module',
+    ecmaVersion: 2019,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
