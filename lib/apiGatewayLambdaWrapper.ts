2f45700ffcab28972ea066099d12a48ebeb8cd78
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2021-02-11 23:27:33 +0200

    feat: define apiGatewayLambda wrapper

---
+++ lib/apiGatewayLambdaWrapper.ts
@@ -0,0 +1,40 @@
+import { APIGatewayEvent, Context, Callback, APIGatewayProxyResult } from 'aws-lambda';
+
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+};
+
+const onSuccesHandler = (
+  data: any,
+  statusCode: number,
+): APIGatewayProxyResult | PromiseLike<APIGatewayProxyResult> => ({
+  statusCode,
+  headers: defaultHeaders,
+  body: JSON.stringify(data),
+});
+
+const onErrorHandler = async (error: Error): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 500,
+    headers: defaultHeaders,
+    body: JSON.stringify(error),
+  };
+};
+
+export const lambdaWrapper = (
+  lambda: (event: APIGatewayEvent, context: Context, callback: Callback) => Promise<[any, number]>,
+  onSucces: (
+    value: any,
+    statusCode: number,
+  ) => APIGatewayProxyResult | PromiseLike<APIGatewayProxyResult> = onSuccesHandler,
+  onError: (error: Error) => Promise<APIGatewayProxyResult> = onErrorHandler,
+) => {
+  return function wrapp(event: APIGatewayEvent, context: Context, callback: Callback): Promise<APIGatewayProxyResult> {
+    return Promise.resolve()
+      .then(() => lambda(event, context, callback))
+      .then(([res, code]: [any, number]) => onSucces(res, code))
+      .catch(onError);
+  };
+};
