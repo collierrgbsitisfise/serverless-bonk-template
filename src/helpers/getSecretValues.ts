ae34457f56f21a61de8c49779f5703d4c1ebf3dc
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2021-02-25 22:28:08 +0200

    feat: help(get secrets)

---
+++ src/helpers/getSecretValues.ts
@@ -0,0 +1,38 @@
+import * as AWS from 'aws-sdk';
+
+const { REGION } = process.env;
+
+const client = new AWS.SecretsManager({
+  region: REGION,
+});
+
+const TIME_INTERVAL = 1000 * 60 * 10; // 10 minutes
+
+const secretsMap: Map<
+  string,
+  {
+    timestamp: number;
+    values: { [index: string]: string };
+  }
+> = new Map();
+
+export const getSecretValue = (secretId: string) => async (...keys: string[]): Promise<string[]> => {
+  const currentTimeStamp = +new Date();
+
+  const isExpired = secretsMap.get(secretId) && currentTimeStamp - secretsMap.get(secretId).timestamp > TIME_INTERVAL;
+
+  if (isExpired) {
+    secretsMap.delete(secretId);
+  }
+
+  if (!secretsMap.get(secretId)) {
+    const { SecretString } = await client.getSecretValue({ SecretId: secretId }).promise();
+    secretsMap.set(secretId, {
+      timestamp: +new Date(),
+      values: JSON.parse(SecretString),
+    });
+  }
+
+  const secrets = secretsMap.get(secretId).values;
+  return keys.map((key: string) => secrets[key]);
+};
