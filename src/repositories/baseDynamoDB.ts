0dfcd5cd619c975769b8825fc5ebf316a1d39230
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2021-02-26 18:45:01 +0200

    chore: capitalize type namings

--- src/repositories/baseDynamoDB.ts
+++ src/repositories/baseDynamoDB.ts
@@ -4,35 +4,35 @@ import { PromiseResult } from 'aws-sdk/lib/request';
 
 const client = new DynamoDB.DocumentClient();
 
-export type getParams = DynamoDB.DocumentClient.GetItemInput;
-export type putParams = DynamoDB.DocumentClient.PutItemInput;
-export type queryParams = DynamoDB.DocumentClient.QueryInput;
-export type updateParams = DynamoDB.DocumentClient.UpdateItemInput;
-export type deleteParams = DynamoDB.DocumentClient.DeleteItemInput;
-export type getResponse = Promise<PromiseResult<DynamoDB.DocumentClient.GetItemOutput, AWS.AWSError>>;
-export type putResponse = Promise<PromiseResult<DynamoDB.DocumentClient.PutItemOutput, AWS.AWSError>>;
-export type queryResponse = Promise<PromiseResult<DynamoDB.DocumentClient.QueryOutput, AWS.AWSError>>;
-export type updateResponse = Promise<PromiseResult<DynamoDB.DocumentClient.UpdateItemOutput, AWS.AWSError>>;
-export type deleteResponse = Promise<PromiseResult<DynamoDB.DocumentClient.DeleteItemOutput, AWS.AWSError>>;
+export type GetParams = DynamoDB.DocumentClient.GetItemInput;
+export type PutParams = DynamoDB.DocumentClient.PutItemInput;
+export type QueryParams = DynamoDB.DocumentClient.QueryInput;
+export type UpdateParams = DynamoDB.DocumentClient.UpdateItemInput;
+export type DeleteParams = DynamoDB.DocumentClient.DeleteItemInput;
+export type GetResponse = Promise<PromiseResult<DynamoDB.DocumentClient.GetItemOutput, AWS.AWSError>>;
+export type PutResponse = Promise<PromiseResult<DynamoDB.DocumentClient.PutItemOutput, AWS.AWSError>>;
+export type QueryResponse = Promise<PromiseResult<DynamoDB.DocumentClient.QueryOutput, AWS.AWSError>>;
+export type UpdateResponse = Promise<PromiseResult<DynamoDB.DocumentClient.UpdateItemOutput, AWS.AWSError>>;
+export type DeleteResponse = Promise<PromiseResult<DynamoDB.DocumentClient.DeleteItemOutput, AWS.AWSError>>;
 
 export class BaseDynamoService {
-  public get(params: getParams): getResponse {
+  public get(params: GetParams): GetResponse {
     return client.get(params).promise();
   }
 
-  public put(params: putParams): putResponse {
+  public put(params: PutParams): PutResponse {
     return client.put(params).promise();
   }
 
-  public query(params: queryParams): queryResponse {
+  public query(params: QueryParams): QueryResponse {
     return client.query(params).promise();
   }
 
-  public update(params: updateParams): updateResponse {
+  public update(params: UpdateParams): UpdateResponse {
     return client.update(params).promise();
   }
 
-  public delete(params: deleteParams): deleteResponse {
+  public delete(params: DeleteParams): DeleteResponse {
     return client.delete(params).promise();
   }
 }
