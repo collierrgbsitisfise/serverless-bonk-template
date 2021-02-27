6ea1bdfb60caed3257196d1645cb9cb0a2926b0d
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2021-02-27 18:30:14 +0200

    handle custome headers for AG lambda wrapper

--- libs/apiGatewayLambdaWrapper.ts
+++ libs/apiGatewayLambdaWrapper.ts
@@ -1,5 +1,7 @@
 import { APIGatewayEvent, Context, Callback, APIGatewayProxyResult } from 'aws-lambda';
 
+type Headers = { [key: string]: string };
+
 const defaultHeaders = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*',
@@ -9,9 +11,13 @@ const defaultHeaders = {
 const onSuccesHandler = (
   data: any,
   statusCode: number,
+  headers?: Headers,
 ): APIGatewayProxyResult | PromiseLike<APIGatewayProxyResult> => ({
   statusCode,
-  headers: defaultHeaders,
+  headers: {
+    ...defaultHeaders,
+    ...headers,
+  },
   body: JSON.stringify(data),
 });
 
@@ -24,17 +30,22 @@ const onErrorHandler = async (error: Error): Promise<APIGatewayProxyResult> => {
 };
 
 export const apiGatewayLambdaWrapper = (
-  lambda: (event: APIGatewayEvent, context?: Context, callback?: Callback) => [any, number] | Promise<[any, number]>,
+  lambda: (
+    event: APIGatewayEvent,
+    context?: Context,
+    callback?: Callback,
+  ) => [any, number, Headers] | Promise<[any, number, Headers]>,
   onSucces: (
     value: any,
     statusCode: number,
+    headers?: Headers,
   ) => APIGatewayProxyResult | PromiseLike<APIGatewayProxyResult> = onSuccesHandler,
   onError: (error: Error) => Promise<APIGatewayProxyResult> = onErrorHandler,
 ) => {
   return function wrapp(event: APIGatewayEvent, context: Context, callback: Callback): Promise<APIGatewayProxyResult> {
     return Promise.resolve()
       .then(() => lambda(event, context, callback))
-      .then(([res, code]: [any, number]) => onSucces(res, code))
+      .then(([res, code, headers]: [any, number, Headers]) => onSucces(res, code, headers))
       .catch(onError);
   };
 };
